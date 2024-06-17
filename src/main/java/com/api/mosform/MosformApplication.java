540d3677b08dcaccf5809406d30d000912e460a6
Horlakz <officialhorlakz@gmail.com>
2024-06-17 09:34:50 +0100

    init

---
+++ src/main/java/com/api/mosform/MosformApplication.java
@@ -0,0 +1,13 @@
+package com.api.mosform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/mosform/MosformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.mosform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
