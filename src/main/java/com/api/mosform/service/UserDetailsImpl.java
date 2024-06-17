1a8f09851de645884bc382a258b9290918ae07b8
Horlakz <officialhorlakz@gmail.com>
2024-06-17 15:23:31 +0100

    feat: creates UserDetailsImpl service

---
+++ src/main/java/com/api/mosform/service/UserDetailsImpl.java
@@ -0,0 +1,44 @@
+package com.api.mosform.service;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.api.mosform.entity.User;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    public String getUserId() {
+        return user.getId().toString();
+    }
+
+}
